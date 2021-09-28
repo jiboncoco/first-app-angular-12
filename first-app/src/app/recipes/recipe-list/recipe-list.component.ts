2ccce464fa65d347054e2b814ff35de622372fd5
Achmad Fauzi <binar-achmadfauzi@Binars-MacBook-Pro.local>
2021-09-28 11:20:19 +0700

    add new project, new component, new model dan slicing design component

--- first-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ first-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://images.unsplash.com/photo-1543353071-c953d88f7033?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1450&q=80'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://images.unsplash.com/photo-1543353071-c953d88f7033?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1450&q=80')
+  ];
 
   constructor() { }
 
