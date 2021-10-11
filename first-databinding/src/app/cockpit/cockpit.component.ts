08b015d25082c61770dfb6e88b5271b2dd3f379d
Achmad Fauzi <binar-achmadfauzi@Binars-MacBook-Pro.local>
2021-10-11 16:35:43 +0700

    update

--- first-databinding/src/app/cockpit/cockpit.component.ts
+++ first-databinding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,27 +8,37 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
 
     @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-    @Output() bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+    @Output('bluePrintCreated') bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
-    newServerName: string = '';
-    newServerContent: string = '';
+    // newServerName: string = '';
+    // newServerContent: string = '';
+
+    // newServerName: string = '';
+    // newServerContent: string = '';
+
+    @ViewChild('serverContentInput')
+    serverContentInput!: ElementRef;
 
     constructor() { }
 
     ngOnInit(): void {
     }
 
-    onAddServer() {
+    onAddServer(nameInput: HTMLInputElement) {
         this.serverCreated.emit({
-            serverName: this.newServerName,
-            serverContent: this.newServerContent
+            // serverName: this.newServerName,
+            serverName: nameInput.value,
+            // serverContent: this.newServerContent
+            serverContent: this.serverContentInput.nativeElement.value
         });
     }
 
-    onAddBlueprint() {
+    onAddBlueprint(nameInput: HTMLInputElement) {
         this.bluePrintCreated.emit({
-            serverName: this.newServerName,
-            serverContent: this.newServerContent
+            // serverName: this.newServerName,
+            serverName: nameInput.value,
+            // serverContent: this.newServerContent
+            serverContent: this.serverContentInput.nativeElement.value
         });
     }
 
--- first-databinding/src/app/server-element/server-element.component.ts
+++ first-databinding/src/app/server-element/server-element.component.ts
@@ -1,18 +1,74 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {    Component, 
+            Input, 
+            OnInit, 
+            OnChanges, 
+            SimpleChanges, 
+            ViewEncapsulation, 
+            AfterContentInit, 
+            AfterContentChecked,
+            AfterViewInit,
+            AfterViewChecked,
+            OnDestroy,
+            ViewChild, 
+            ElementRef } from '@angular/core';
 
 @Component({
     selector: 'app-server-element',
     templateUrl: './server-element.component.html',
-    styleUrls: ['./server-element.component.css']
+    styleUrls: ['./server-element.component.css'],
+    encapsulation: ViewEncapsulation.Emulated // None, Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements 
+    OnInit, OnChanges, 
+    AfterContentInit, AfterContentChecked,
+    AfterViewInit, AfterViewChecked,
+    OnDestroy {
 
     @Input('srvElement')
     element!: { type: string; name: string; content: string; };
 
-    constructor() { }
+    @Input()
+    name!: string;
 
-    ngOnInit(): void {
+    @ViewChild('heading')
+    header!: ElementRef;
+
+    constructor() {
+        console.log('constructor called !');
     }
 
+    ngOnChanges(changes: SimpleChanges) {
+        console.log('constructor called!');
+        console.log(changes);
+    }
+
+    ngOnInit(): void {
+        console.log('ngOnit called !');
+        console.log('Text Content : ' + this.header.nativeElement.textContent);
+    }
+
+    ngDoCheck() {
+        console.log('ngDoCheck called!');
+    }
+
+    ngAfterContentInit() {
+        console.log('ngAfterContentInit called!');
+    }
+
+    ngAfterContentChecked() {
+        console.log('ngAfterContentChecked called!');
+    }
+
+    ngAfterViewInit() {
+        console.log('ngAfterViewInit called!');
+        console.log('Text Content : ' + this.header.nativeElement.textContent);
+    }
+
+    ngAfterViewChecked() {
+        console.log('ngAfterViewChecked called!');
+    }
+
+    ngOnDestroy() {
+        console.log('ngOnDestroy called!');
+    }
 }
